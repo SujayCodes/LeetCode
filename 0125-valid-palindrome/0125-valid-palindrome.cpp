9d4db3109cb453b0d6c596444a096b7ec36c3550
SujayCodes <sujaychand5732@gmail.com>
2026-08-27 18:06:39 +0530

    Time: 0 ms (100%), Space: 10.7 MB (7.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        string res = "";
-        
-        for (int i = 0; i < s.size(); i++) {
-            if (isalnum(s[i])) {
+        string res= "";
+        for(int i=0;i<s.size();i++){
+            if(isalnum(s[i])){
                 res += tolower(s[i]);
             }
         }
-        
-        string reversed = res;
-        
 
-        reverse(reversed.begin(), reversed.end());
-        
-
-        return res == reversed;
+        string rev = res;
+        reverse(rev.begin(),rev.end());
+        return res==rev;
     }
 };
