6e57495f47993c76560f958c20337a8258cf5dc7
SujayCodes <sujaychand5732@gmail.com>
2026-09-03 19:13:32 +0530

    Time: 0 ms (100%), Space: 9.3 MB (84.38%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-
-        vector<int> ans;
-
-        int top = 0;
-        int bottom = matrix.size() - 1;
-
         int left = 0;
-        int right = matrix[0].size() - 1;
+        int top = 0;
+        int right = matrix[0].size()-1;
+        int bottom = matrix.size()-1;
+        vector<int> res;
 
-        while (top <= bottom && left <= right) {
-
-            // 1. Traverse top row → 
-            for (int j = left; j <= right; j++) {
-                ans.push_back(matrix[top][j]);
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                res.push_back(matrix[top][i]);
             }
             top++;
 
-
-            // 2. Traverse right column ↓
-            for (int i = top; i <= bottom; i++) {
-                ans.push_back(matrix[i][right]);
+            for(int i=top;i<=bottom;i++){
+                res.push_back(matrix[i][right]);
             }
             right--;
 
 
-            // 3. Traverse bottom row ←
-            // Check because the bottom row may already be traversed
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    ans.push_back(matrix[bottom][j]);
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    res.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
 
-
-            // 4. Traverse left column ↑
-            // Check because the left column may already be traversed
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    ans.push_back(matrix[i][left]);
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    res.push_back(matrix[i][left]);
                 }
                 left++;
             }
+            
         }
-
-        return ans;
+        return res;
     }
 };
