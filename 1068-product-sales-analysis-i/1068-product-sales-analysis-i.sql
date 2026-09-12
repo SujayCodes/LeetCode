03d449dbe6f38b3856192e61794d356508ee68ab
Sujay Chand <sujaychand5732@gmail.com>
2026-09-12 22:23:19 +0530

    Time: 1166 ms (95.59%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,7 +1,5 @@
-# Write your MySQL query statement below
-
-SELECT p.product_name, s.year, s.price
-FROM Sales s
-LEFT JOIN
-Product p
-ON s.product_id = p.product_id;
+SELECT e2.product_name, e1.year, e1.price
+FROM Sales e1
+INNER JOIN Product e2
+ON e1.product_id = e2.product_id
+;
