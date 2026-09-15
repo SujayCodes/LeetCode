ac5ed0ad749d094392f25cec691b76379641071e
Sujay Chand <sujaychand5732@gmail.com>
2026-09-15 17:35:28 +0530

    Time: 443 ms (8.74%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+# Approach :: MAKE A NEW TABLE WITH EXCLUDING THE MAX SLARY ROW AND THEN FIND THE MAX SALARY AMONG THE REST TABLE
+
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE 
+salary NOT IN (SELECT MAX(salary) from Employee)
+;
